4cb4678fc8f713519426e4fa842003877f980aa7
aaba13210 <aaba13210@gmail.com>
2022-05-30 13:10:14 -0300

    Carga

--- JS/Script.js
+++ JS/Script.js
@@ -1,3 +1,4 @@
+/*Definicion de variables para el formulario*/
 const nombre = document.getElementById("myname");
 const apellidos = document.getElementById("surname");
 const correo = document.getElementById("email");
@@ -23,34 +24,41 @@ function validacionForm() {
     element.lastElementChild.innerHTML = "";
   });
 
+  /*Condicion campo nombre vacío*/ 
   if (nombre.value.length < 1 || nombre.value.trim() == "") {
     mostrarMensajeError("myname", "Nombre no valido*");
     condicion = false;
   }
+  /*Condicion campo apellido vacío*/ 
   if (apellidos.value.length < 1 || apellidos.value.trim() == "") {
     mostrarMensajeError("surname", "Apellido no valido");
     condicion = false;
   }
+  /*Condicion campo mail vacío*/ 
   if (correo.value.length < 1 || correo.value.trim() == "") {
     mostrarMensajeError("email", "Correo no valido*");
     condicion = false;
   }
+  /*Condicion campo celular vacio o que no tiene 10 caracters*/ 
   if (
-    celular.value.length != 9 ||
+    celular.value.length != 10 ||
     celular.value.trim() == "" ||
     isNaN(celular.value)
   ) {
     mostrarMensajeError("mobile", "Celular no valido*");
     condicion = false;
   }
+  /*Condicion campo contraseña vacío*/ 
   if (contrasenia.value.length < 1 || contrasenia.value.trim() == "") {
     mostrarMensajeError("password", "Contraseña no valido*");
     condicion = false;
   }
+  /*Condicion campo contraseña distinto*/ 
   if (contrasenia2.value != contrasenia.value) {
-    mostrarMensajeError("repeatPassword", "Contraseña error*");
+    mostrarMensajeError("repeatPassword", "Contraseñas no coinciden");
     condicion = false;
   }
+  /*Chequea marca de la casilla de terminos y condiciones*/
   if (!terminosYcondiciones.checked) {
     mostrarMensajeError("termsAndConditions", "Acepte*");
     condicion = false;
@@ -64,7 +72,7 @@ function mostrarMensajeError(claseInput, mensaje) {
   let elemento = document.querySelector(`.${claseInput}`);
   elemento.lastElementChild.innerHTML = mensaje;
 }
-
+/*Muesta mensaje cuando es correcto el formulario*/
 function enviarFormulario() {
   form.reset();
   form.lastElementChild.innerHTML = "Listo !!";
